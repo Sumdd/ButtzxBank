5f7a0927f72fcd34cf56752cd1cd32076de7080a
Administrator <Administrator@DESKTOP-3VP0P8Q>
2021-01-08 15:15:28 +0800

    添加项目文件。

---
+++ ButtzxBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ButtzxBank.Startup))]
+namespace ButtzxBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
